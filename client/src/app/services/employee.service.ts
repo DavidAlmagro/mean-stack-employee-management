09621ec8e2925b61e409abe8140ed4a8b9860ac2
David Almagro <davalmcab@gmail.com>
2025-12-29 07:07:21 +0100

    Update API URL to use Render backend

--- client/src/app/services/employee.service.ts
+++ client/src/app/services/employee.service.ts
@@ -7,7 +7,7 @@ import { Employee } from '../models/employee.model';
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiUrl = 'http://localhost:5200/employees';
+  private apiUrl = 'https://employee-management-api-7uel.onrender.com/employees';
 
   constructor(private http: HttpClient) { }
 
